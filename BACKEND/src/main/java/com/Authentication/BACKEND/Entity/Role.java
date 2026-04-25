efc52e4e06209a114adb35e24c78e91c17d27172
Hirusha Dilshan Alwis <dilshanhirusha093@gmail.com>
2026-04-25 14:55:20 +0530

    remove Role enum

--- BACKEND/src/main/java/com/Authentication/BACKEND/Entity/Role.java
+++
@@ -1,9 +0,0 @@
-package com.Authentication.BACKEND.Entity;
-
-public enum Role {
-    ROLE_ADMIN,
-    ROLE_USER,
-    ROLE_TICKET_MANAGER,
-    ROLE_BOOKING_MANAGER,
-    ROLE_FACILITIES_MANAGER
-}
