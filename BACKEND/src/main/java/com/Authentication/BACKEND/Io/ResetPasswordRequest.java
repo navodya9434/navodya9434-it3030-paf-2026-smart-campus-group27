9f406b656f23c8709c7b8378df6e71d1bc069485
Hirusha Dilshan Alwis <dilshanhirusha093@gmail.com>
2026-04-25 14:23:34 +0530

    create ResetPasswordRequest class with validation constraints

---
+++ BACKEND/src/main/java/com/Authentication/BACKEND/Io/ResetPasswordRequest.java
@@ -0,0 +1,21 @@
+package com.Authentication.BACKEND.Io;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResetPasswordRequest {
+
+    @NotBlank(message = "New password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
+     private String newPassword;
+    @NotBlank(message = "OTP is required")
+     private String otp;
+    @NotBlank(message = "Email is required")
+     private String email;
+}
