e4388c3fd17a3613c571d2539663bd6031a1c089
Hirusha Dilshan Alwis <dilshanhirusha093@gmail.com>
2026-04-25 14:26:08 +0530

    implement JwtRequestFilter for token-based authentication

---
+++ BACKEND/src/main/java/com/Authentication/BACKEND/Filter/JwtRequestFilter.java
@@ -0,0 +1,90 @@
+package com.Authentication.BACKEND.Filter;
+
+
+import com.Authentication.BACKEND.Service.AppUserDetailsService;
+import com.Authentication.BACKEND.Util.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+
+@Component
+@RequiredArgsConstructor
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    private final AppUserDetailsService appUserDetailsService;
+    private final JwtUtil jwtUtil;
+
+        private static final List<String> PUBLIC_URLS = List.of(
+            "/login",
+            "/admin/login",
+            "/register",
+            "/send-reset-otp",
+            "/reset-password",
+            "/logout"
+        );
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String path = request.getServletPath();
+        if (path.startsWith("/oauth2/")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        if(PUBLIC_URLS.contains(path)) {
+              filterChain.doFilter(request, response);
+              return;
+        }
+        String jwt = null;
+        String email = null;
+
+        //Check the authorization header
+        final String authorizationHeader = request.getHeader("Authorization");
+        if(authorizationHeader != null && authorizationHeader.startsWith("Bearer")) {
+              jwt = authorizationHeader.substring(7);
+        }
+
+        //If not found in header, check the cookies
+        if(jwt == null) {
+            Cookie[] cookies = request.getCookies();
+            if(cookies != null) {
+                 for(Cookie cookie: cookies) {
+                      if("jwt".equals(cookie.getName())){
+                          jwt = cookie.getValue();
+                          break;
+                      }
+                 }
+
+
+            }
+        }
+
+        //Validate the token and set security context
+
+        if(jwt != null) {
+             email = jwtUtil.extractEmail(jwt);
+             if(email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                  UserDetails userDetails =  appUserDetailsService.loadUserByUsername(email);
+                  if(jwtUtil.validateToken(jwt, userDetails)) {
+                      UsernamePasswordAuthenticationToken authenticationToken =
+                              new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                      authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                      SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                  }
+             }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
