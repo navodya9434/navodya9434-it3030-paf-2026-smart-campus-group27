2e5ac3edae3f3418a1fc9328b36f8df5f0fb358e
Hirusha Dilshan Alwis <dilshanhirusha093@gmail.com>
2026-04-25 14:10:07 +0530

    implement verifyOtp method with OTP validation and account activation

--- BACKEND/src/main/java/com/Authentication/BACKEND/Service/ProfileServiceImpl.java
+++ BACKEND/src/main/java/com/Authentication/BACKEND/Service/ProfileServiceImpl.java
@@ -121,6 +121,30 @@ public class ProfileServiceImpl implements ProfileService {
 
     }
 
+     @Override
+    public void verifyOtp(String email, String otp) {
+
+        UserEntity existingUser = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + email));
+
+        // Check OTP valid or not
+        if (existingUser.getVerifyOtp() == null || !existingUser.getVerifyOtp().equals(otp)) {
+            throw new RuntimeException("Invalid OTP");
+        }
+
+        // Check expiry
+        if (existingUser.getVerifyOtpExpiredAt() < System.currentTimeMillis()) {
+            throw new RuntimeException("OTP Expired");
+        }
+
+        // Mark as verified
+        existingUser.setIsAccountVerified(true);
+        existingUser.setVerifyOtp(null);
+        existingUser.setVerifyOtpExpiredAt(0L);
+
+        userRepository.save(existingUser);
+    }
+
 
 
 
