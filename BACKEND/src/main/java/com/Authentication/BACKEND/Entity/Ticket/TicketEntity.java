3e1ee7597e9bcc39eb6312d45d1fc0c4ef64ffd2
navodya9434 <navodprint@gmail.com>
2026-04-25 11:27:27 +0530

    add TicketEntity with user relations, status handling, and timestamps

---
+++ BACKEND/src/main/java/com/Authentication/BACKEND/Entity/Ticket/TicketEntity.java
@@ -0,0 +1,69 @@
+package com.Authentication.BACKEND.Entity.Ticket;
+
+import com.Authentication.BACKEND.Entity.UserEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Table(name = "tickets")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TicketEntity {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String category;
+
+    @Column(length = 2000)
+    private String description;
+
+    private String location;
+
+    private String priority;
+
+    private String status;
+
+    private String rejectionReason;
+
+    private String contactEmail;
+    private String contactPhone;
+
+    @ManyToOne
+    @JoinColumn(name = "created_by")
+    private UserEntity createdBy;
+
+    @ManyToOne
+    @JoinColumn(name = "assigned_to")
+    private UserEntity assignedTo;
+
+    @ElementCollection
+    private List<String> imageUrls = new ArrayList<>();
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.status = "OPEN";
+    }
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
