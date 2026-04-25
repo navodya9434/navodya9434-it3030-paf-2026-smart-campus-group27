09907be9dbe2b2cd0b139ccdde26a9943fa5c37c
Hirusha Dilshan Alwis <dilshanhirusha093@gmail.com>
2026-04-25 14:55:44 +0530

    add Role enum for user role management

---
+++ BACKEND/src/main/java/com/Authentication/BACKEND/Entity/Role.java
@@ -0,0 +1,9 @@
+package com.Authentication.BACKEND.Entity;
+
+public enum Role {
+    ROLE_ADMIN,
+    ROLE_USER,
+    ROLE_TICKET_MANAGER,
+    ROLE_BOOKING_MANAGER,
+    ROLE_FACILITIES_MANAGER
+}
