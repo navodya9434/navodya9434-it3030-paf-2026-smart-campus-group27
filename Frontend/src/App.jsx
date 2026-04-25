7e18ae55bfe6e7a4ef949ab8493b10b727802137
Hirusha Dilshan Alwis <dilshanhirusha093@gmail.com>
2026-04-25 18:15:16 +0530

    add App component with conditional navbar/footer hiding

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import Login from "./pages/Login";
+import VerifyEmail from "./pages/VerifyEmail";
+import OAuth2Success from "./pages/OAuth2Success";
+
+const App = () => {
+
+  const user = JSON.parse(localStorage.getItem("user"));
+  const location = useLocation(); // React Router hook
+  const hideNavbarPaths = ["/login", "/oauth2/success", "/verify-email"];
+  const hideFooterPaths = ["/login", "/oauth2/success", "/verify-email"];
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
