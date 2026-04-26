66a7c5bc3a12b469f438e6501865d5f76a90cd31
navodya9434 <navodprint@gmail.com>
2026-04-27 00:03:47 +0530

    ticket alert servise file

---
+++ BACKEND/src/main/java/com/Authentication/BACKEND/Service/Ticket/TicketAlertService.java
@@ -0,0 +1,15 @@
+package com.Authentication.BACKEND.Service.Ticket;
+
+import com.Authentication.BACKEND.Io.Ticket.TicketAlertResponse;
+
+import java.util.List;
+
+public interface TicketAlertService {
+
+    void sendTicketAlert(Long ticketId);
+
+    List<TicketAlertResponse> getAlertsForRole(String role);
+
+    void resolveAlert(Long alertId);
+    void resolveAlertsByTicketId(Long ticketId);
+}
