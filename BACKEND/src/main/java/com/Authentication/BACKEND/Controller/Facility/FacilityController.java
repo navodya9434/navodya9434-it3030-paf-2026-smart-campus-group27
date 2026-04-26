c4d0687a8487de1f1a51c83ed0a23acf52bb1336
HMTP-Herath <tharukaherath30@gmail.com>
2026-04-26 22:56:05 +0530

    Add FacilityController to manage facility-related operations

---
+++ BACKEND/src/main/java/com/Authentication/BACKEND/Controller/Facility/FacilityController.java
@@ -0,0 +1,18 @@
+package com.Authentication.BACKEND.Controller.Facility;
+
+import com.Authentication.BACKEND.Entity.Facility.FacilityStatus;
+import com.Authentication.BACKEND.Entity.Facility.FacilityType;
+import com.Authentication.BACKEND.Io.Facility.FacilityRequest;
+import com.Authentication.BACKEND.Io.Facility.FacilityResponse;
+import com.Authentication.BACKEND.Service.Facility.FacilityService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
