96904b7de3531971ef458362742c4a8f190b8ec5
sachini12-wq <sachinidulmini81@gmail.com>
2026-04-27 09:07:48 +0530

    booking entity file

--- BACKEND/src/main/java/com/Authentication/BACKEND/Entity/Booking/BookingEntity.java
+++ BACKEND/src/main/java/com/Authentication/BACKEND/Entity/Booking/BookingEntity.java
@@ -1,12 +1,24 @@
 package com.Authentication.BACKEND.Entity.Booking;
 
-import com.Authentication.BACKEND.Entity.Facility.FacilityEntity;
-import com.Authentication.BACKEND.Entity.UserEntity;
-import jakarta.persistence.*;
-import lombok.*;
-
 import java.time.LocalDateTime;
 
+import com.Authentication.BACKEND.Entity.Facility.FacilityEntity;
+import com.Authentication.BACKEND.Entity.UserEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+//booking entity file
 @Entity
 @Table(name = "bookings")
 @Getter
